22d0f572fd8d1057515f99cb90898587a8614f3f
Kayla Dale <kmeyers119@gmail.com>
2024-03-02 10:26:08 -0600

    OAuth 2.0 lab completed- non working

---
+++ src/main/java/edu/mccneb/codeschool/oauthlab/OauthLabApplication.java
@@ -0,0 +1,13 @@
+package edu.mccneb.codeschool.oauthlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/mccneb/codeschool/oauthlab/OauthLabApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mccneb.codeschool.oauthlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
